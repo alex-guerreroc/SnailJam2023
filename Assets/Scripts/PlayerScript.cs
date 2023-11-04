ae2169413cff07cf303d27a2e7daa66825036666
alex-guerreroc <a01198336@tec.mx>
2023-11-04 11:24:13 -0600

    Inicio

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour
+{
+    public int speed;
+    public int jumpForce;
+
+    private Rigidbody2D rb;
+    private Vector2 direction;
+
+    public enum groundState {Grounded, Air};
+    public groundState currentState = groundState.Air;
+
+    private GameObject standingOn = null;
+
+    [SerializeField] private Transform groundCheck;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), 0);
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jump();
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * speed, rb.velocity.y);
+    }
+
+    void getGrounded(){
+        Collider2D groundCol = Physics2D.OverlapArea(new Vector2(groundCheck.position.x + 0.5f,groundCheck.position.y + 0.1f), new Vector2(groundCheck.position.x + -0.5f,groundCheck.position.y - 0.1f));
+
+        if(groundCol != null)
+        {
+            standingOn = groundCol.gameObject;
+            currentState = groundState.Grounded;
+        }
+        else
+        {
+            standingOn = null;
+            currentState = groundState.Air;
+        }
+    }
+
+    void jump(){
+        getGrounded();
+        Vector2 power = new Vector2(0,0);
+        if(currentState == groundState.Grounded)
+        {
+            power = Vector2.up * jumpForce;
+            Debug.Log(power);
+            standingOn.GetComponent<Rigidbody2D>().AddForce(-power, ForceMode2D.Impulse); //Adds force to the object that it is standing on, for physical accuracy. Newton's 3rd Law ;)
+        }
+        rb.AddForce(power, ForceMode2D.Impulse); //Makes the jump
+    }
+
+}
