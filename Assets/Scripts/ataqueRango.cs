30e72fe869c00893c01a071d941e88ec62b9b701
Kuleii <147216070+Kuleii@users.noreply.github.com>
2023-11-05 16:01:58 -0600

    sistema de ataque

---
+++ Assets/Scripts/ataqueRango.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ataqueRango : MonoBehaviour
+{
+    public float offset;
+    public GameObject projectile;
+    public Transform shotPoint;
+    private float timeBtwShots;
+    public float startTimeBtwShots;
+    // Start is called before the first frame update
+ private void Update()
+    {
+        // Handles the weapon rotation
+        Vector3 difference = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float rotZ = Mathf.Atan2(difference.y, difference.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(0f, 0f, rotZ + offset);
+
+        if (timeBtwShots <= 0)
+        {
+            if (Input.GetKey(KeyCode.X))
+            {
+                Debug.Log("Has shot!");
+                Instantiate(projectile, shotPoint.position, transform.rotation);
+                timeBtwShots = startTimeBtwShots;
+            }
+        }
+        else {
+            timeBtwShots -= Time.deltaTime;
+        }
+
+       
+    }
+
+    // Update is called once per frame
+
+}
