1652e2853a963c27cfb026806ab987029257f39b
alex-guerreroc <a01198336@tec.mx>
2023-11-05 22:00:26 -0600

    Final

---
+++ Assets/portalend.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class portalend : MonoBehaviour
+{
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the player has collided with the portal
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //debug message
+            Debug.Log("Player has collided with the portal");
+            // Load the next level
+            SceneManager.LoadScene(0);
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
